fe282f3b62f0561fb04f312f07cfdf1b056ee546
rodes115 <rodesreyes115@gmail.com>
2025-05-18 17:25:53 -0600

    pequeña mod

--- Secc_25_58_UpTask/Uptask_backend/src/controllers/ProjectController.ts
+++ Secc_25_58_UpTask/Uptask_backend/src/controllers/ProjectController.ts
@@ -50,14 +50,17 @@ export class ProjectController {
     static updateProject = async (req:Request, res:Response) =>{
         const {id} = req.params
         try {
-           const project = await Project.findByIdAndUpdate(id, req.body)
+           const project = await Project.findById(id)
 
            if(!project){
                 const error = new Error('Proyecto no encontrado')
                 res.status(404).json({error:error.message})
             }
 
-           await project?.save()
+           project.clientName = req.body.clientName
+           project.projectName = req.body.projectName
+           project.description = req.body.description
+           await project.save()
            res.send('Proyecto Actualizado Correctamente')
         } catch (error) {
             console.log(error)
