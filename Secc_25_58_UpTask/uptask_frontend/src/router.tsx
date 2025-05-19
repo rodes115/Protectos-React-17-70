698754e177bfaad29f83e9ebd1bb1ddf958a7427
rodes115 <rodesreyes115@gmail.com>
2025-05-19 00:11:15 -0600

    Sección 29 UpTask -Administrador de Tareas - Creando Frontend de la aplicacion | CAP 483 - 487

---
+++ Secc_25_58_UpTask/uptask_frontend/src/router.tsx
@@ -0,0 +1,18 @@
+import {BrowserRouter,Route,Routes} from 'react-router-dom'
+import AppLayout from '@/layouts/AppLayout'
+import DashboardView from '@/views/DashboardView'
+
+
+
+export default function Router(){
+
+     return (
+          <BrowserRouter>
+               <Routes>
+                    <Route element={<AppLayout/>}>
+                         <Route path='/' element={<DashboardView/>}/>
+                    </Route>
+               </Routes>
+          </BrowserRouter>
+     )
+}
