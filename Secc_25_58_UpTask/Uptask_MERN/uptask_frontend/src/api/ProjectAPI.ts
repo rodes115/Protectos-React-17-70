fe4ca79dcf3c922846b39d03d9da8877eefb5c31
rodes115 <rodesreyes115@gmail.com>
2025-05-19 21:54:07 -0600

    Sección 31 : UpTask - Administrador de Tareas - Frontend Dashboard de Proyectos | CAP 500 - 502

--- Secc_25_58_UpTask/Uptask_MERN/uptask_frontend/src/api/ProjectAPI.ts
+++ Secc_25_58_UpTask/Uptask_MERN/uptask_frontend/src/api/ProjectAPI.ts
@@ -1,14 +1,31 @@
 import api from "@/lib/axios"
-import type { ProjectFormData } from "../types"
+import { dashboardProjectSchema, type ProjectFormData } from "../types"
 import { isAxiosError } from "axios"
 
-export async function createProject(formData : ProjectFormData){
+export async function createProject(formData: ProjectFormData) {
     try {
-        const {data} = await api.post('/projects', formData)
+        const { data } = await api.post('/projects', formData)
         return data
     } catch (error) {
-        if(isAxiosError(error) && error.response){
+        if (isAxiosError(error) && error.response) {
             throw new Error(error.response.data.error)
-        }  
+        }
+    }
+}
+
+export async function getProjects() {
+    try {
+
+        const { data } = await api('/projects')
+        const response = dashboardProjectSchema.safeParse(data)
+        if (response.success) {
+            return response.data
+        }
+
+    } catch (error) {
+        if (isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error)
+        }
+
     }
 }
